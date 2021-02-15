c0ade76c704d6d201f7787955dd3e27a8e1e17bd
Veks678 <Imeranor.Python@mail.ru>
2021-02-15 11:03:52 +0500

    Calculator 2.2

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from interface_parameters import Param_widgets, Param_window, Param_frames
 
 class main:
     def __init__(self):
-        self.__list_frames = []
-
         self.__label_widgets_list = ['Expression','Decision','7','8','9',\
                                      '+','4','5','6','-','1','2','3','*',\
                                      '←','0','C','/','=','.']
+        self.__list_frames = []
+        self.__index_frame = 0
 
         self.__object_windows = Param_window()
 
         for label_line in range(7):
             self.__list_frames.append(Param_frames(label_line).call_frame())
 
-        for widget_label in self.__label_widgets_list:
-            self.__object_widgets = Param_widgets(widget_label, \
-                                                  self.__list_frames)
-            self.__object_widgets.call_widgets()
+        for label_line in range(19):
+            if label_line in (1,2,6,10,14,18):
+                self.__index_frame += 1
+
+            Param_widgets(self.__label_widgets_list[label_line], \
+                          self.__list_frames[self.__index_frame]\
+                          ).call_widgets()
 
         self.__object_windows.call_window()
 
